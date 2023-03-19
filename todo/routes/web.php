148af43bfef48b0d3460fccdef5fe3e82c17e231
tosho-oliver <t.furukawa@binc.jp>
2023-03-19 18:57:43 +0900

    feat: タスク一覧を追加

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index']);
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');;
