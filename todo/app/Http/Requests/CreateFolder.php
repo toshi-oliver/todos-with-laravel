b122325b8f7d2200b8cfd5a24527703535255af9
tosho-oliver <t.furukawa@binc.jp>
2023-04-02 08:50:19 +0900

    feat: フォルダ追加機能の実装

---
+++ todo/app/Http/Requests/CreateFolder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateFolder extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:20',
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+     public function attributes(): array
+    {
+     return [
+         'title' => ' フォルダ名',
+     ];
+    }
+}
--- todo/routes/web.php
+++ todo/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');;
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class, 'create']);
